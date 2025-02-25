2a4bd53bdba290a8d8bd43bc733e52514301d126
dtfiedler <dylan@ar.io>
2025-02-25 10:44:39 -0700

    fix(gql): add try/catch to avoid blowups

--- src/gql.ts
+++ src/gql.ts
@@ -72,82 +72,95 @@ export class GQLEventPoller implements EventPoller {
     let cursor;
     let hasNextPage = true;
     let lastBlockHeight = await this.getLastBlockHeight();
-    while (hasNextPage) {
-      this.logger.info(`Fetching events from block height ${lastBlockHeight}`, {
-        cursor,
-        hasNextPage,
-        lastBlockHeight,
-        minBlockHeight: lastBlockHeight,
-      });
-      const query = eventsFromProcessGqlQuery({
-        processId: this.processId,
-        cursor: cursor,
-        authorities: this.authorities,
-        minBlockHeight: lastBlockHeight,
-      });
-      const response = await fetch(this.gqlUrl, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body: query,
-      });
-      if (!response.ok) {
-        this.logger.error(
-          `Error fetching events from block height ${lastBlockHeight}`,
-          response,
+
+    try {
+      while (hasNextPage) {
+        this.logger.info(
+          `Fetching events from block height ${lastBlockHeight}`,
+          {
+            cursor,
+            hasNextPage,
+            lastBlockHeight,
+            minBlockHeight: lastBlockHeight,
+          },
         );
-        break;
-      }
-
-      const data = await response.json();
-
-      // parse the nodes to get the id
-      if (data?.data?.transactions?.edges?.length === 0) {
-        this.logger.info(`No events found for block height ${lastBlockHeight}`);
-        break;
-      }
-
-      this.logger.info(
-        `Found ${data?.data?.transactions?.edges?.length} events`,
-      );
-
-      // now get all the events
-      const events = data?.data?.transactions?.edges || [];
-      const sortedEvents = [...events].sort((a: any, b: any) => {
-        return a?.node?.tags
-          .find((t: { name: string; value: string }) => t.name === 'Reference')
-          ?.value.localeCompare(
-            b?.node?.tags.find(
-              (t: { name: string; value: string }) => t.name === 'Reference',
-            )?.value ?? '',
+        const query = eventsFromProcessGqlQuery({
+          processId: this.processId,
+          cursor: cursor,
+          authorities: this.authorities,
+          minBlockHeight: lastBlockHeight,
+        });
+        const response = await fetch(this.gqlUrl, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: query,
+        });
+        if (!response.ok) {
+          this.logger.error(
+            `Error fetching events from block height ${lastBlockHeight}`,
+            response,
           );
-      });
-      // sort events by reference
-      for (const event of sortedEvents) {
-        // fetch the transaction from arweave
-        const eventResult = await this.arweave.api.get(event.node.id);
-        const gqlEvent = {
-          id: event.node.id,
-          tags: event.node.tags,
-          data: eventResult.data,
-        };
-        // process the raw event, don't await
-        this.processor.processGQLEvent(gqlEvent);
+          break;
+        }
+
+        const data = await response.json();
+
+        // parse the nodes to get the id
+        if (data?.data?.transactions?.edges?.length === 0) {
+          this.logger.info(
+            `No events found for block height ${lastBlockHeight}`,
+          );
+          break;
+        }
+
+        this.logger.info(
+          `Found ${data?.data?.transactions?.edges?.length} events`,
+        );
+
+        // now get all the events
+        const events = data?.data?.transactions?.edges || [];
+        const sortedEvents = [...events].sort((a: any, b: any) => {
+          return a?.node?.tags
+            .find(
+              (t: { name: string; value: string }) => t.name === 'Reference',
+            )
+            ?.value.localeCompare(
+              b?.node?.tags.find(
+                (t: { name: string; value: string }) => t.name === 'Reference',
+              )?.value ?? '',
+            );
+        });
+        // sort events by reference
+        for (const event of sortedEvents) {
+          // fetch the transaction from arweave
+          const eventResult = await this.arweave.api.get(event.node.id);
+          const gqlEvent = {
+            id: event.node.id,
+            tags: event.node.tags,
+            data: eventResult.data,
+          };
+          // process the raw event, don't await
+          this.processor.processGQLEvent(gqlEvent);
+        }
+        // update the cursor
+        lastBlockHeight = Math.max(
+          lastBlockHeight,
+          events?.[0]?.node?.block?.height ?? 0,
+        );
+        cursor = events?.[0]?.cursor;
+        hasNextPage = data?.data?.transactions?.pageInfo?.hasNextPage ?? false;
+        this.updateLastBlockHeight(lastBlockHeight + 1);
       }
-      // update the cursor
-      lastBlockHeight = Math.max(
-        lastBlockHeight,
-        events?.[0]?.node?.block?.height ?? 0,
+    } catch (error) {
+      this.logger.error('Error fetching events', error);
+    } finally {
+      this.fetching = false;
+      this.logger.info(
+        `Finished fetching events up to block height ${lastBlockHeight}`,
       );
-      cursor = events?.[0]?.cursor;
-      hasNextPage = data?.data?.transactions?.pageInfo?.hasNextPage ?? false;
-      this.updateLastBlockHeight(lastBlockHeight + 1);
     }
-    this.fetching = false;
-    this.logger.info(
-      `Finished fetching events up to block height ${lastBlockHeight}`,
-    );
   }
 
   updateLastBlockHeight(blockHeight: number): void {
