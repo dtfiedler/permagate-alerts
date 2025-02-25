183b48d95904ad14be74425bb599b2da4caffdba
dtfiedler <dylan@ar.io>
2025-02-25 10:43:37 -0700

    fix(gql): add optional checks

--- src/gql.ts
+++ src/gql.ts
@@ -141,7 +141,7 @@ export class GQLEventPoller implements EventPoller {
         events?.[0]?.node?.block?.height ?? 0,
       );
       cursor = events?.[0]?.cursor;
-      hasNextPage = data.data.transactions.pageInfo.hasNextPage;
+      hasNextPage = data?.data?.transactions?.pageInfo?.hasNextPage ?? false;
       this.updateLastBlockHeight(lastBlockHeight + 1);
     }
     this.fetching = false;
