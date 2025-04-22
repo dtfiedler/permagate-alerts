9e0a87902095ee9fa83c08f3ef44040f2cdcacda
dtfiedler <dylan@ar.io>
2025-04-22 13:21:58 -0500

    fix(ao): add back ao config

--- src/lib/ao.ts
+++ src/lib/ao.ts
@@ -1,3 +1,9 @@
 import { ARIO } from '@ar.io/sdk';
+import { connect } from '@permaweb/aoconnect';
+import * as config from '../config.js';
 
 export const ario = ARIO.mainnet();
+export const ao = connect({
+  CU_URL: config.cuUrl,
+  MODE: 'legacy',
+});
